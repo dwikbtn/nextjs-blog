b8c9df33510710ee198f5d3db85b729c32bc0381
dwikbtn <dwikbtn15@gmail.com>
2021-08-05 22:33:51 +0800

    finish the latest part style

--- components/news/News.jsx
+++ components/news/News.jsx
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
 import style from "./news.module.scss";
+import Image from "next/image";
 
-function News(props) {
-  console.log(props.allPosts);
+function News({ allPosts }) {
+  const clamped =
+    allPosts[0].context.split(" ").slice(0, 30).join(" ") + " ...";
+  console.log(clamped);
+  const articleLatest = (
+    <div className={style["article-latest"]}>
+      <div className={style["article-text"]}>
+        <div className={style["article-title"]}>
+          <h1>{allPosts[0].title}</h1>
+        </div>
+        <div className={style["article-content"]}>
+          <p>{clamped}</p>
+        </div>
+        <div className={style["article-footer"]}>
+          <div className={style.date}>{allPosts[0].date}</div>
+          <div className={style["read-more"]}>Read more</div>
+        </div>
+      </div>
+      <div className={style["image-container-latest"]}>
+        <Image
+          src={allPosts[0].imageURL}
+          alt={allPosts[0].title}
+          width={435}
+          height={245}
+        />
+      </div>
+    </div>
+  );
 
   const article = (
     <div className="article">
@@ -20,7 +47,7 @@ function News(props) {
   );
   return (
     <article className={"containers grid-container " + style.news}>
-      <div className={"grid-bg " + style["news-latest"]}>{article}</div>
+      <div className={"grid-bg " + style["news-latest"]}>{articleLatest}</div>
       <div className={style["news-panel"]}>Test</div>
       <div className={style["news-panel"]}>Test</div>
       <div className={style["news-panel"]}>Test</div>
